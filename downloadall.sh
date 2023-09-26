5426652c0093b10db82e56c917fe72e8fc1d942c
S22F5 <s22f5@mailfence.com>
2023-09-26 21:17:55 +0200

    changes3

--- downloadall.sh
+++ downloadall.sh
@@ -1,1 +1,9 @@
-sed 's/^/$1\//g' *.txt | xargs wget -P downloaded
+#!/bin/bash
+#check for arguments
+if [ -z "$1" ]
+  then
+      printf "Usage: ./downloadall.sh i.supa.codes(url)\n"
+    exit 1
+fi
+
+sed "s/^/$1\//g" -- *.txt | xargs wget -P downloaded
