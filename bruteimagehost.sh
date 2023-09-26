85627d4b8edc59e92e597491ee482153cdb6d854
S22F5 <s22f5@mailfence.com>
2023-09-26 21:14:12 +0200

    changes2

--- bruteimagehost.sh
+++ bruteimagehost.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 shopt -s lastpipe
 #check for arguments
 if [ -z "$3" ]
@@ -10,11 +10,11 @@ fi
 #info header output
 clear
 #------------
-printf "”””””””””””””””””””””””””””\n"
+printf ' ”””””””””””””””””””””””””””\n '
 printf "\033[1mbruteimagehost.sh\033[0m\n"
-printf "”””””””””””””””””””””””””””\n"
+printf ' ”””””””””””””””””””””””””””\n '
 #
-printf "$1 | s22f3\n"
+printf "%s | s22f3\n" "$1"
 #
 printf "bruteforcing press \033[1mq\033[0m to quit\n\n"
 #-------------
@@ -23,27 +23,27 @@ printf "bruteforcing press \033[1mq\033[0m to quit\n\n"
     case "$*" in
         *"-d"*)
             #just download all
-            until read -s -d q -t 0.01; do 
+            until read -r -s -d q -t 0.01; do 
                 status_code=200
-                random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9_-' | head -n1 -c$2)
+                random=$(tr -dc 'a-zA-Z0-9_-'  < /dev/urandom | head -n1 -c"$2")
                 wget "https://$1/$random" -c -q -P  downloaded | status_code=404
-                printf "%s\n" "$status_code"
+                printf "%s:%s\n" "$status_code" "$random"
             done
             ;;
 
         *"-v"*)
             #verbose output
-            until read -s -d q -t 0.01; do 
-                random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9_-' | head -n1 -c$2)
-                curl --no-progress-meter -I "https://$1/$random" | head -n1 | tail -c6 | head -c3 | tee -a $3 && printf ":$random\n" | tee -a $3
+            until read -r -s -d q -t 0.01; do 
+                random=$(tr -dc 'a-zA-Z0-9_-'  < /dev/urandom | head -n1 -c"$2")
+                curl --no-progress-meter -I "https://$1/$random" | head -n1 | tail -c6 | head -c3 | tee -a "$3" && printf ":%s\n" "$random" | tee -a "$3"
             done
             ;;
 
         *)
             #normal output
-            until read -s -d q -t 0.01; do 
-                random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9_-' | head -n1 -c$2)
-                curl --no-progress-meter -I "https://$1/$random" | head -n 1 | grep -q "200" && printf "%s\n" $random | tee -a $3
+            until read -r -s -d q -t 0.01; do 
+                random=$(tr -dc 'a-zA-Z0-9_-'  < /dev/urandom | head -n1 -c"$2")
+                curl --no-progress-meter -I "https://$1/$random" | head -n 1 | grep -q "200" && printf "%s\n" "$random" | tee -a "$3"
             done
             ;;
 esac
